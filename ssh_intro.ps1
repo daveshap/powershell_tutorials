46e15334e2f8040878d8cb0997e5c646ca52969e
David Shapiro <daveshap37@gmail.com>
2019-04-16 11:08:49 -0400

    Update ssh_intro.ps1

--- ssh_intro.ps1
+++ ssh_intro.ps1
@@ -1,3 +1,6 @@
+# IP address of an SSH server
+$ip_addr = "192.168.0.10"
+
 # import the SSH module for PowerShell
 Import-Module Posh-SSH
 
@@ -5,7 +8,7 @@ Import-Module Posh-SSH
 if ($creds -eq $null) { $creds = Get-Credential -Message "Enter credentials for Linux machines" }
 
 # connect to an SSH session
-$ssh = New-SSHSession -ComputerName "192.168.0.10" -Credential $creds -AcceptKey 
+$ssh = New-SSHSession -ComputerName $ip_addr -Credential $creds -AcceptKey 
 
 # run a command in the SSH session
 $cmd = Invoke-SSHCommand -Command "time" -SSHSession $ssh
