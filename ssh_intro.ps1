bca074d372617284a0d7ed30f0b45387a7c681f7
David Shapiro <daveshap37@gmail.com>
2019-04-16 10:50:53 -0400

    Create ssh_intro.ps1

---
+++ ssh_intro.ps1
@@ -0,0 +1,17 @@
+# import the SSH module for PowerShell
+Import-Module Posh-SSH
+
+# if credentials are not in memory, ask for credentials
+if ($creds -eq $null) { $creds = Get-Credential -Message "Enter credentials for Linux machines" }
+
+# connect to an SSH session
+$ssh = New-SSHSession -ComputerName "192.168.0.10" -Credential $creds -AcceptKey 
+
+# run a command in the SSH session
+$cmd = Invoke-SSHCommand -Command "time" -SSHSession $ssh
+
+# show the results of the command
+$cmd.Output
+
+# destroy SSH session
+Remove-SSHSession -SSHSession $ssh
